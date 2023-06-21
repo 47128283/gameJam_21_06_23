15611e96606db3f738878c82e8b222ae42f78f38
Ivan Machamer <ivan.machamer@students.mq.edu.au>
2023-06-21 15:07:37 +1000

    21/06/2023

--- game/src/RoomTile.java
+++ game/src/RoomTile.java
@@ -1,18 +1,80 @@
-import java.util.Random;
-public class RoomTile {
-    private boolean isSafe; //means that this cell would be along the intended path
-    public char drawnType;
-    private char actualType; 
-    public char itemType;
-    Random r = new Random();
+import java.awt.*;
 
-    RoomTile(boolean s, char dt, char at, Character it){ //given type
-        
+class RoomTile extends Rectangle {
+    // RoomTile properties
+    protected static int size = RoomGrid.RoomTileSize;
+    protected char col;
+    protected int row;
+    protected final Color DEFAULT_COLOR = new Color(235,235,235);
+    protected Color bgColor;
+    protected String description;
+
+    // // tile container
+    // protected Tile tile;
+
+    public RoomTile(char inCol, int inRow, int inX, int inY) {
+        super(inX, inY, size, size);
+        col = inCol;
+        row = inRow;
+        // set the default bgColor
+        bgColor = DEFAULT_COLOR;
     }
 
-    RoomTile(){ //randomly generated
-        actualType = (char)r.nextInt(15);
-        drawnType = 0;
+    // handle RoomTile background
+    void paintRoomTileBackground(Graphics g, Point mousePos){
+        if (contains(mousePos)) {
+            g.setColor(Color.GRAY);
+        } else {
+            g.setColor(bgColor);
+        }
+        g.fillRect(x, y, size, size);
+    }
 
+    // handle Tile Polygons
+    void paintTile(Graphics g, Point mousePos){
+        // // tbh just ignore dead tiles
+        // if(tile != null){
+        //     tile.paint(g,x,y,width,height);
+        // }
+
+    }
+    // handle base outline
+    void paintRoomTileOutline(Graphics g, Point mousePos){
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, size, size);
+    }
+
+    // general tile painter method called from Grid class
+    void paint(Graphics g, Point mousePos) {
+        // handle RoomTile background
+        paintRoomTileBackground(g,mousePos);
+
+        // handle Tile Polygons
+        paintTile(g,mousePos);
+
+        // now draw outline
+        paintRoomTileOutline(g,mousePos);
+
+    }
+
+    @Override
+    public boolean contains(Point p) {
+        if (p != null) {
+            return (super.contains(p));
+        } else {
+            return false;
+        }
+    }
+
+    public int leftOfComparison(RoomTile c) {
+        return Character.compare(col, c.col);
+    }
+
+    public int aboveComparison(RoomTile c) {
+        return Integer.compare(row, c.row);
+    }
+
+    public String toString() {
+        return Character.toString(col) + Integer.toString(row) + ":" + description;
     }
 }
