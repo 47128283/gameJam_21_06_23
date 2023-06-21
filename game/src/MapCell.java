aa59ea1a150cc2047aaecb0d8807ca76f62c4748
Ivan Machamer <ivan.machamer@students.mq.edu.au>
2023-06-21 15:28:47 +1000

    Update MapCell.java

--- game/src/MapCell.java
+++ game/src/MapCell.java
@@ -8,12 +8,18 @@ class MapCell{
     public int row;
     public int col;
 
+    class walkable extends MapCell{
+        MapCell();
+    }
+
     MapCell(long s,char t, int c, int r) {
         final Random rand = new Random(s);
         if(t == 0) {
-            
+
         }
     }
+
+    MapCell() {}
     void paint(Graphics g) {
       
     }
