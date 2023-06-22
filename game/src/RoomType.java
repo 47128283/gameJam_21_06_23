27dcab144faa79c327576d138ec07a6825356c3b
Corbeau <aurora.griffith@students.mq.edu.au>
2023-06-22 15:35:44 +1000

    Update RoomType.java
    forgot to comment out

--- game/src/RoomType.java
+++ game/src/RoomType.java
@@ -1,41 +1,41 @@
-import java.awt.*;
-public abstract class RoomType {
-    public String rooms;
-    RoomType() {
-        rooms = "";
-    }
+// import java.awt.*;
+// public abstract class RoomType {
+//     public String rooms;
+//     RoomType() {
+//         rooms = "";
+//     }
     
-    RoomType(String input) {
-        rooms = input;
-    }
-}
+//     RoomType(String input) {
+//         rooms = input;
+//     }
+// }
 
-final class LShape extends RoomType {
-    LShape() {
-        super("#*##**###");
-    }
-}
+// final class LShape extends RoomType {
+//     LShape() {
+//         super("#*##**###");
+//     }
+// }
 
-final class BackwardsLShape extends RoomType {
-    BackwardsLShape() {
-        super("#*#**####");
-    }
-}
+// final class BackwardsLShape extends RoomType {
+//     BackwardsLShape() {
+//         super("#*#**####");
+//     }
+// }
 
-final class TShape extends RoomType {
-    TShape() {
-        super("###***#*#");
-    }
-}
+// final class TShape extends RoomType {
+//     TShape() {
+//         super("###***#*#");
+//     }
+// }
 
-final class PlusShape extends RoomType {
-    PlusShape() {
-        super("#*#***#*#");
-    }
-}
+// final class PlusShape extends RoomType {
+//     PlusShape() {
+//         super("#*#***#*#");
+//     }
+// }
 
-final class LineShape extends RoomType {
-    LineShape() {
-        super("#*##*##*#");
-    }
-}
+// final class LineShape extends RoomType {
+//     LineShape() {
+//         super("#*##*##*#");
+//     }
+// }
